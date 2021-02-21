30836e94456d1599da621b286750c0599c4757d4
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2021-02-20 19:43:26 -0800

    Separate message content constraints to general string constraint

--- src/model/context.rs
+++ src/model/context.rs
@@ -1,5 +1,10 @@
-use typemap_rev::TypeMap;
+use aho_corasick::AhoCorasick;
+use dashmap::DashMap;
 
+/// This is shared to be accessed in the rules parsing.
+/// Created each time an event fires.
+#[derive(Debug, Clone)]
 pub struct Context {
-    pub data: TypeMap,
+    /// Guild specific word lists
+    pub word_lists: DashMap<String, AhoCorasick>,
 }
--- src/model/rule.rs
+++ src/model/rule.rs
@@ -1,9 +1,24 @@
-use crate::model::Condition;
+use crate::model::{Condition, Context};
 use serde::{Deserialize, Serialize};
-use twilight_model::gateway::event::EventType;
+use std::error::Error;
+use twilight_model::gateway::event::{DispatchEvent, EventType};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Rule {
-    pub triggers: Vec<EventType>,
+    pub trigger: EventType,
     pub conditions: Condition,
+}
+
+impl Rule {
+    pub async fn check_event(
+        &self,
+        event: &DispatchEvent,
+        context: &Context,
+    ) -> Option<Result<bool, Box<dyn Error>>> {
+        if event.kind() != self.trigger {
+            return None;
+        }
+
+        Some(self.conditions.check_event(event, context).await)
+    }
 }
